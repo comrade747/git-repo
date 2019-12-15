c5cf77734f3af185fc181f751fd9f0f08e23f52f
Andrey Ershov <ershovandrey@yahoo.com>
2019-12-15 17:14:01 +0300

    Homework 2

--- Lesson2/main.py
+++ Lesson2/main.py
@@ -9,11 +9,83 @@ Created on Wed Dec 11 22:21:15 2019
 from models import Base, Good, Category, Stock, CategoriesGoods, StocksGoods
 from database import x5rgDb
 from datetime import datetime, timezone
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome import service
+from selenium.webdriver.opera.options import Options
+from selenium.webdriver.common import desired_capabilities
+from selenium.webdriver.opera import options
     
+
+def createBrowser ():
+    _operaDriverLoc = os.path.abspath(r'c:\browser_drivers\operadriver_win32\operadriver.exe')  # Replace this path with the actual path on your machine.
+    _operaExeLoc = os.path.abspath(r'C:\Program Files\Opera\65.0.3467.72\opera.exe')   # Replace this path with the actual path on your machine.
+
+    webdriver_service = service.Service(_operaDriverLoc)
+    webdriver_service.start()
+    _remoteExecutor = webdriver_service.service_url
+    _operaCaps = desired_capabilities.DesiredCapabilities.OPERA.copy()
+
+    _operaOpts = options.ChromeOptions()
+    _operaOpts._binary_location = _operaExeLoc
+
+    # Use the below argument if you want the Opera browser to be in the maximized state when launching. 
+    # The full list of supported arguments can be found on http://peter.sh/experiments/chromium-command-line-switches/
+    _operaOpts.add_argument('--start-maximized')  
+
+#    driver = webdriver.Remote(_remoteExecutor, 
+#                              options = _operaOpts, 
+#                              desired_capabilities = _operaCaps)
+    driver = webdriver.Chrome(executable_path = _operaDriverLoc, 
+                              chrome_options = _operaOpts, 
+                              desired_capabilities = _operaCaps)
+
+    driver.get('https://5ka.ru/special_offers/')
+    return driver
+    
+    
+def fillGoods (driver, db):
+
+    catName = "Акционные товары"
+    cat = Category(catName, "ВСЕ Акционные товары")
+    rows = db.session.query(Category).filter_by(name=catName).all()
+    if len(rows) == 0:
+        db.session.add(cat)
+    else:
+        cat = rows[0]
+    
+#    elements = driver.find_elements_by_css_selector('div.sale-card__footer')
+    elements = WebDriverWait(driver, timeout=5). \
+        until(lambda x: x.find_elements_by_css_selector('div.sale-card__description-wrapper'))
+#    elements = driver.find_elements_by_css_selector('div.sale-card__description-wrapper')
+    for element in elements:
+        name = element.find_element_by_css_selector('p.sale-card__title')
+        date = element.find_element_by_css_selector('span.sale-card__date')
+        good = Good(name.text, name.text, name.text)
+        
+        rows = db.session.query(Good).filter_by(name=name.text).all()
+        if len(rows) == 0:
+            cat.goods.append(good)
+
+    db.session.transaction.commit()
+#        print(f'{name.text} : {date.text}')
+
 if __name__ == '__main__':
     connString = 'sqlite:///x5rg.sqlite'
     db = x5rgDb(Base, connString)
+
+    cat = Category("Акционные товары", "ВСЕ Акционные товары")
+    db.session.add(cat)
     
+    driver = createBrowser()
+    
+    fillGoods (driver, db)
+
+    time.sleep(5) #see the result
+    if driver is not None:
+        driver.quit()
 #    good = Good("кефир", "Кефир Простоквашино 3.2%", "ООО Простоквашино")
 #    cat = Category("кисломолочка", "Кисломолочные продукты")
 #    db.session.add(cat)
@@ -49,5 +121,6 @@ if __name__ == '__main__':
 #    cat_good = db.session.query(CategoriesGoods).all()
 #    print(cat_good)
     
-    stk_good = db.session.query(StocksGoods).all()
+    stk_good = db.session.query(CategoriesGoods).all()
     print(stk_good)
+    
