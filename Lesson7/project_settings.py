0f44ce87c9dc45d26d1336d981104bfab27e1974
Andrey Ershov <ershovandrey@yahoo.com>
2020-01-19 14:28:13 +0300

    Homework 7

---
+++ Lesson7/project_settings.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Common settings for Lesson7 project
+#
+
+DB_NAME = 'geekbrains'
+DB_HOST = 'raspberry'
+#DB_HOST = 'gbubuntu.tk'
+DB_USER = 'sysdba'
+DB_PWRD = 'masterkey'
+
+TSRCT_PATH = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+ROOT_PATH = r'C:\Users\andre\source\repos\git-repo\CollectProcessData\Lesson7\СКД_Поверка_весов'
